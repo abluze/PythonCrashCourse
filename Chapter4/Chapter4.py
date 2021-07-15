2506ad171c04c826ec99dc2d8d03c1da20d771a4
fiveblur <87116628+fiveblur@users.noreply.github.com>
2021-07-15 07:14:48 +0000

    main chapter 4 file edit

--- Chapter4/Chapter4.py
+++ Chapter4/Chapter4.py
@@ -1,1 +1,17 @@
-print("hello")
+#players = ['alex','karl','clay','george','niki']
+#print(players[0:5:2])
+#print("Godly cheers in the AU include (but are not limited to):")
+#for player in players[-2:]:
+ # print(player.title())
+
+my_foods = ['pizza','not falafel', 'something']
+friends_foods = my_foods[:]
+
+my_foods.append('human flesh')
+friends_foods.append('beef')
+
+print("My favourite foods are:")
+print(my_foods)
+
+print(f"\nMy friend's favourite foods are:")
+print(friends_foods)
