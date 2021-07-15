074207a956307e0c74a3cdb3569444457709f000
fiveblur <87116628+fiveblur@users.noreply.github.com>
2021-07-15 07:29:56 +0000

    4-10

---
+++ Chapter4/4-10.py
@@ -0,0 +1,8 @@
+random_items = ['table','chair','laptop','apple','orange','bowl','charger','mat','pencil','pen']
+print(len (random_items))
+print("The first three items in the list are:")
+print(random_items[:3])
+print("Three items from the middle of the list are:")
+print(random_items[4:6])
+print("The last three items in the list are:")
+print(random_items[-3:])
