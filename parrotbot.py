3825cf20407dd9d558365e9143084e9c9d9567fc
abluze <raniasuttonpasta@gmail.com>
2021-09-15 13:27:26 +0800

    commit

--- parrotbot.py
+++ parrotbot.py
@@ -14,6 +14,17 @@ async def on_ready():
     print(f'{bot.user} has connected to Discord!')
     print(f'Connected to {bot.guilds}')
 
+async def on_ready(self):
+  if not self.ready:
+    self.ready = True
+    print("bot ready")
+  
+    channel = self.get_channel(854202868188708875)
+    await channel.send("greetings.")
+
+
+  print(f'message received: {msg}')
+
 @bot.event
 async def on_message(message):
   print(f'message from:{message.author} received by:{bot.user}')
@@ -25,9 +36,9 @@ async def on_message(message):
   print(f'message received: {msg}')
 
   if msg == 'hello':
-    await message.channel.send(f'hello, {message.author.name}')
+    await message.channel.send(f'hello, {message.author.display_name}')
   elif msg == 'bye':
-    await message.channel.send(f'farewell, {message.author.name}')
+    await message.channel.send(f'farewell, {message.author.display_name}')
 
   # do a google search for msg 
   # find the first image, and respond with that image 
