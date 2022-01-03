567268e3cc0aa0909c091812fc9b6b57808eb5b8
abluze <raniasuttonpasta@gmail.com>
2022-01-03 13:37:32 +0800

    continued chap. 6

--- Chapter6/Chapter6.py
+++ Chapter6/Chapter6.py
@@ -1,5 +1,37 @@
-ring = {'colour': 'silver'}
-print(f"The colour of the ring is {ring['colour']}.")
-ring['colour'] = 'black'
-print(f"The colour of the ring is now {ring['colour']}.")
-print(ring)
+#ring = {'colour': 'silver'}
+#print(f"The colour of the ring is {ring['colour']}.")
+#ring['colour'] = 'black'
+#print(f"The colour of the ring is now {ring['colour']}.")
+#print(ring)
+
+people = {
+    'athena':'bad',
+    'zoe':'bad',
+    'tejas':'bad',
+    'daisy':'good',
+    'ada':'good',
+    'greed':'horrible',
+    'delta':'pathetic',
+    'ego.town':'bad',
+    'dumbfounded':'ok',
+    'oshiroo':'disgusting',
+    'pathetic':'pathetic',
+    'faint':'bad',
+}
+
+athena_goodness = people['athena']
+zoe_goodness = people['zoe']
+tejas_goodness = people['tejas']
+daisy_goodness = people['daisy']
+ada_goodness = people['ada']
+greed_goodness = people['greed']
+delta_goodness = people['delta']
+ego_town_goodness = people['ego.town']
+dumbfounded_goodness = people['dumbfounded']
+oshiroo_goodness = people['oshiroo']
+pathetic_goodness = people['pathetic']
+faint_goodness = people['faint']
+
+
+print(f"daisy is {daisy_goodness}, and tejas is {tejas_goodness}.")
+print(f"all of athena's usernames suck. for example, greed is {greed_goodness}, delta is {delta_goodness}, ego.town is {ego_town_goodness}, oshiroo is {oshiroo_goodness}, pathetic is, well, {pathetic_goodness}, and faint is {faint_goodness}.")
