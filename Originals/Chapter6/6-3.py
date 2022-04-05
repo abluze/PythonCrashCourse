883c5ba76aee944f350b2a5ccd0617daf330752b
abluze <raniasuttonpasta@gmail.com>
2022-04-05 16:37:51 +0800

    Restarting

---
+++ Originals/Chapter6/6-3.py
@@ -0,0 +1,10 @@
+words = {
+    'chair':'something you sit on''often made of wood, metal or plastic',
+    'hair':'dead cells (usually coloured) that sit on your head',
+    'pencilcase':'something you store stationery in (usually a bag or box)',
+    'cat':'a small feline with fur',
+    'goose':'a small, fat animal that hangs around water'
+}
+
+print(words['chair'])
+
