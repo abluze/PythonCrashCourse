990eb11903636b34ddb6d85db0de3261f5453a09
abluze <raniasuttonpasta@gmail.com>
2022-04-07 23:22:10 +0800

    Short Session

---
+++ 2022/Retake/Chapter3/3-10.py
@@ -0,0 +1,18 @@
+list = ['notebook', 'pen', 'lamp', 'poster', 'scissors', 'ruler', 'marker', 'pencil', 'clock', 'keyboard', 'mouse', 'mousepad']
+
+print(list[0])
+print(list[-1])
+
+print(f"\n")
+
+print(sorted(list))
+print(sorted(list, reverse=True))
+
+print(f"\n")
+
+print(len(list))
+
+print(f"\n")
+
+list.append('monitor')
+list.insert(0, 'paper')
